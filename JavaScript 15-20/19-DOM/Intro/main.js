dcb9b08117f1e88ec053f6067b6a687ec35db5d0
AlexGomezCota <alejandrogomezcota@gmail.com>
2023-05-28 16:57:24 -0600

    intro a dom

---
+++ JavaScript 15-20/19-DOM/Intro/main.js
@@ -0,0 +1,13 @@
+const parrafo = document.getElementById("text")
+console.log(parrafo.innerHTML) //tambien se puede .innerText
+
+parrafo.innerText = "Texto actualizado"
+console.log(parrafo.innerText)
+
+parrafo.innerHTML = "<li>Elemento 1</li> <li> Elemento2 </li>"
+
+
+
+
+
+
